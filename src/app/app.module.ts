2d9e59a938d7421586eae39eb4826f6db889e190
Guilherme Lopes <guilherme@netsoft.eti.br>
2020-04-18 00:13:44 -0300

    instalação de depedencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/moment';
 import * as moment from 'moment';
 
+import 'core-js';
+
 export function momentAdapterFactory() {
   return adapterFactory(moment);
 };
