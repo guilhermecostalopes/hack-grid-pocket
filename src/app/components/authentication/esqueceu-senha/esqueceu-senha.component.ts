d2406c2d798793074ef86e5b7daf1dbf6c65a9a8
Guilherme Lopes <guilherme@netsoft.eti.br>
2020-04-19 13:42:27 -0300

    ajustes

--- src/app/components/authentication/esqueceu-senha/esqueceu-senha.component.ts
+++ src/app/components/authentication/esqueceu-senha/esqueceu-senha.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-esqueceu-senha',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EsqueceuSenhaComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group ( {
+      email: [ null, Validators.compose( [ Validators.required ] ) ]
+    } );
   }
 
+  onSubmit() {
+    this.router.navigate ( ['/authentication/login'] );
+  }
 }
