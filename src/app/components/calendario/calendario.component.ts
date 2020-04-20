7ac71f36ab7b2c0176959baa4ffbe7eb95f22258
Guilherme Lopes <guilherme@netsoft.eti.br>
2020-04-19 21:30:01 -0300

    ajustes

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -18,21 +18,17 @@ export class CalendarioComponent  {
   calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
   calendarWeekends = true;
   calendarEvents: EventInput[] = [
-    { title: 'Event Now', start: new Date() }
+    { title: 'Reunião', start: new Date() },
+    { title: 'Reunião', start: new Date("2020-04-20T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-21T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-22T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-23T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-24T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-25T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-26T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-27T20:56:00Z") },
+    { title: 'Reunião', start: new Date("2020-04-28T20:56:00Z") },
   ];
-
-  toggleVisible() {
-    this.calendarVisible = !this.calendarVisible;
-  }
-
-  toggleWeekends() {
-    this.calendarWeekends = !this.calendarWeekends;
-  }
-
-  gotoPast() {
-    let calendarApi = this.calendarComponent.getApi();
-    calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
-  }
 
   handleDateClick(arg) {
     if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
@@ -43,6 +39,4 @@ export class CalendarioComponent  {
       })
     }
   }
-
-
 }
